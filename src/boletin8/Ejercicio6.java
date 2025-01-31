ec56c7f99a34a599d71dd21442f4f7feb7c4f171
angelgarciaguillena <angel.garcia@iesnervion.es>
2025-01-31 14:10:23 +0100

    Ejercicio 6 incompleto

--- src/boletin8/Ejercicio6.java
+++ src/boletin8/Ejercicio6.java
@@ -1,5 +1,6 @@
 package boletin8;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Scanner;
@@ -8,10 +9,10 @@ public class Ejercicio6 {
 	public static void main(String[] args) {
 		
 		/*Creamos un mapa para almacenar los nombres y sus telefonos*/
-		LinkedHashMap<String, LinkedHashSet<String>> nombres = new LinkedHashMap<String, LinkedHashSet<String>>();
+		LinkedHashMap<String, HashSet<Integer>> nombres = new LinkedHashMap<String, HashSet<Integer>>();
 		
 		/*Creamos una coleccion para almacenar y editar los numeros de telefono*/
-		LinkedHashSet<Integer> numeros = new LinkedHashSet<Integer>();
+		HashSet<Integer> numeros;
 		
 		/*Creamos una variable para almacenar la eleccion del usuario*/
 		int eleccion;
@@ -59,29 +60,96 @@ public class Ejercicio6 {
 				/*Leemos el nombre introducido por el usuario*/
 				nombre = sc.nextLine();
 				
+				/*Si la persona ya esta agregada se le avisa al usuario sino se añade el nombre al mapa*/
+				if(nombres.containsKey(nombre)) {
+					System.out.println("La persona ya existe");
+					
+				} else {
+					
+					/*Añadimos el nombre al mapa*/
+					nombres.put(nombre, new HashSet<>());
+				}
+			}
+			
+			/*Si la opcion es 2 añadimos un telefono a una persona que ya estaba en la lista*/
+			case 2 ->{
+				
+				/*Pedimos al usuario que introduzca el nombre al que se le quiere añadir el numero*/
+				System.out.println("Introduce un nombre");
+				
+				/*Leemos el nombre introducido por el usuario*/
+				nombre = sc.nextLine();
+				
 				/*Pedimos al usuario que introduzca el numero de telefono de la persona*/
-				System.out.println("Introduce su numero de telefono");
+				System.out.println("Introduce el numero de telefono");
 				
 				/*Leemos el numero de telefono de la persona*/
 				telefono = sc.nextInt();
-			}
-			
-			/*Si la opcion es 2 añadimos otro telefono a una persona que ya estaba en la lista*/
-			case 2 ->{
 				
+				/*Almacenamos el telefono el conjunto asociado a nombre*/
+				numeros = nombres.get(nombre);
+				
+				/*Si el numero se añade notificamos al usuario que el telefono se ha añadido correctamente sino mostramos que no se
+				 * ha añadido*/
+				if(numeros.add(telefono)) {
+					System.out.println("El telefono se ha añadido correctamente");
+				} else {
+					System.out.println("El telefono no se ha añadido correctamente");
+				}
 			}
 			
 			/*Si la opcion es 3 mostramos los telefonos de una persona*/
 			case 3 ->{
 				
+				/*Pedimos al usuario que introduzca el nombre que quiere buscar*/
+				System.out.println("Introduce un nombre");
 				
+				/*Leemos el nombre introducido por el usuario*/
+				nombre = sc.nextLine();
+				
+				/*Si el nombre se encuentra en el mapa y no esta vacio mostraremos el nombre y el telefono al usuario sino se
+				 * notificara que no se encuentra en el mapa*/
+				if(nombres.containsKey(nombre) && !nombres.get(nombre).isEmpty()) {
+					System.out.println("El telefono de " + nombre + " es: " + nombres.get(nombre));
+				} else {
+					System.out.println("La persona no se encuentra en la lista");
+				}
 			}
 			
 			/*Si la opcion es 4 eliminamos un telefono de una persona*/
 			case 4 ->{
 				
+				/*Pedimos al usuario que introduzca el nombre al que pertenece el numero que quiere eliminar*/
+				System.out.println("Introduce un nombre");
 				
+				/*Leemos el nombre introducido por el usuario*/
+				nombre = sc.nextLine();
+				
+				/*Si el nombre se encuentra en el mapa*/
+				if(nombres.containsKey(nombre)) {
+					
+					/*Pedimos al usuario que introduzca el numero de telefono que quiere eliminar*/
+					System.out.println("Introduce el numero de telefono");
+					
+					/*Leemos el numero de telefono de la persona*/
+					telefono = sc.nextInt();
+					
+					/*Guardamos el numero en una nueva lista*/
+					numeros = nombres.get(nombre);
+					
+					/*Si el telefono se elimina se notifica al usuario que se ha eliminado sino se le avisa que no se ha realizado*/
+					if(numeros.remove(telefono)) {
+						System.out.println("El telefono se ha eliminado correctamente");
+					} else {
+						System.out.println("El telefono no se ha eliminado");
+					}
+				} else {
+					
+					/*Notificamos al usuario que la persona no se encuentra en el mapa*/
+					System.out.println("La persona no se encuentra en la lista");
+				}
 			}
+			
 			/*Si es 5 eliminamos a la persona y todos sus numeros*/
 			case 5 ->{
 				
@@ -110,7 +178,5 @@ public class Ejercicio6 {
 		
 		/*Cerramos el Scanner*/
 		sc.close();
-		
-		
 	}
 }
